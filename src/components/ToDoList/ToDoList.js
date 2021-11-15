64926c8b1c2ed76bac879fc392c4912b55c51375
Silviu Iordache <silviu.iordache92@gmail.com>
2021-11-15 13:44:51 +0200

    Add check input

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -10,14 +10,17 @@ export default class ToDoList extends React.Component {
     this.props.clearList();
   }
   render() {
-      const items = this.props.items?.map((item, index) => 
-        <p key={index}>{index + 1}. {item}</p>
+      const items = this.props.items?.map((item, index) =>
+        <div key={index} className='d-flex align-items-center'>
+          <p className='mb-0'>{index + 1}. {item}</p>
+          <input type='checkbox' className='ms-2'></input>
+        </div> 
       );
       return (
         <div className="card p-3">
           <h2 className="mb-2">Items</h2>
           {items}
-          <button onClick={this.clearList}> Clear list</button>
+          <button className='mt-3' onClick={this.clearList}> Clear list</button>
         </div>
       )
     }
