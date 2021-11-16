6cd57bd2cacc85278073e32b6683a9397739393e
Silviu Iordache <silviu.iordache92@gmail.com>
2021-11-16 22:34:04 +0200

    Extract listitems as fcomp

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -14,21 +14,28 @@ export default class ToDoList extends React.Component {
     this.props.deleteItem(event);
   }
 
-  render() {
+  ListItems = () => {
     const items = this.props.items?.map((item, index) =>
-      <ToDoListItem 
-        index={index}
-        item={item}
-        key={index}
-        toggleCompletion={this.toggleCompletion}
-        deleteItem={this.deleteItem}
-      />
-    );
+    <ToDoListItem 
+      index={index}
+      item={item}
+      key={index}
+      toggleCompletion={this.toggleCompletion}
+      deleteItem={this.deleteItem}
+    />
+  );
+  return items
+  }
+  render() {
+   
     return (
       <div className="card p-3">
-        <h2 className="mb-2">Items</h2>
-        {items}
-        <button className='mt-3' onClick={this.clearList}>Clear list</button>
+        <h2 className="mb-2 d-flex justify-content-between mb-4">
+          <span>Items</span>
+          <span>1/3</span>
+        </h2>
+        {this.ListItems()}
+        <button className='mt-4' onClick={this.clearList}>Clear list</button>
       </div>
     )
   }
