0a13ab8ef01ff3a338fc5365e696c970737c0f8b
Silviu Iordache <silviu.iordache92@gmail.com>
2021-11-16 22:09:50 +0200

    Add functions and make them arrow

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -2,13 +2,7 @@ import React from 'react';
 import ToDoListItem from '../ToDoListItem/ToDoListItem.js'
 
 export default class ToDoList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.clearList = this.clearList.bind(this);
-    this.toggleCompletion = this.toggleCompletion.bind(this);
-  }
-
-  clearList() {
+  clearList = () => {
     this.props.clearList();
   }
 
@@ -16,17 +10,26 @@ export default class ToDoList extends React.Component {
     this.props.toggleCompletion(event);
   }
 
+  deleteItem = (event) => {
+    this.props.deleteItem(event);
+  }
+
   render() {
-      const items = this.props.items?.map((item, index) =>
-        <ToDoListItem index={index} item={item} key={index} toggleCompletion={this.toggleCompletion}/>
-      );
-      return (
-        <div className="card p-3">
-          <h2 className="mb-2">Items</h2>
-          {items}
-          <button className='mt-3' onClick={this.clearList}>Clear list</button>
-        </div>
-      )
-    }
-  
+    const items = this.props.items?.map((item, index) =>
+      <ToDoListItem 
+        index={index}
+        item={item}
+        key={index}
+        toggleCompletion={this.toggleCompletion}
+        deleteItem={this.deleteItem}
+      />
+    );
+    return (
+      <div className="card p-3">
+        <h2 className="mb-2">Items</h2>
+        {items}
+        <button className='mt-3' onClick={this.clearList}>Clear list</button>
+      </div>
+    )
+  }
 }
