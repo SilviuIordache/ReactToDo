34af38071f33a410ade8529000e4dbffe849d4bd
Silviu Iordache <silviu.iordache92@gmail.com>
2021-11-16 22:36:24 +0200

    Extract the rest in function components

--- src/components/ToDoList/ToDoList.js
+++ src/components/ToDoList/ToDoList.js
@@ -14,28 +14,40 @@ export default class ToDoList extends React.Component {
     this.props.deleteItem(event);
   }
 
+  ListHeader = () => {
+    return (
+      <h2 className="mb-2 d-flex justify-content-between mb-4">
+        <span>Items</span>
+        <span>1/3</span>
+      </h2>
+    )
+  }
+
   ListItems = () => {
     const items = this.props.items?.map((item, index) =>
-    <ToDoListItem 
-      index={index}
-      item={item}
-      key={index}
-      toggleCompletion={this.toggleCompletion}
-      deleteItem={this.deleteItem}
-    />
-  );
-  return items
+      <ToDoListItem 
+        index={index}
+        item={item}
+        key={index}
+        toggleCompletion={this.toggleCompletion}
+        deleteItem={this.deleteItem}
+      />
+    );
+    return items;
   }
+
+  ListClear = () => {
+    return (
+      <button className='mt-4' onClick={this.clearList}>Clear list</button>
+    )
+  }
+
   render() {
-   
     return (
       <div className="card p-3">
-        <h2 className="mb-2 d-flex justify-content-between mb-4">
-          <span>Items</span>
-          <span>1/3</span>
-        </h2>
+        {this.ListHeader()}
         {this.ListItems()}
-        <button className='mt-4' onClick={this.clearList}>Clear list</button>
+        {this.ListClear()}
       </div>
     )
   }
